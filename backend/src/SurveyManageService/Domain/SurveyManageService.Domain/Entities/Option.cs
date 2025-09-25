9af9a28f204184cb991f9fea8098fb48dca0653e
Daniil Mikrukov <elmikrukov@outlook.com>
2025-09-25 10:11:58 +0500

    Реализованы Entity для Survey сервисов

---
+++ backend/src/SurveyManageService/Domain/SurveyManageService.Domain/Entities/Option.cs
@@ -0,0 +1,9 @@
+﻿namespace SurveyManageService.Domain.Entities;
+
+public class Option
+{
+    public string Title { get; set; }
+    public string Value { get; set; }
+    public int Order { get; set; }
+    public bool IsCorrect { get; set; }
+}
